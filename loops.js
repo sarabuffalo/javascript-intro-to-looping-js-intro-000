558b92524a9293067eb6c6e64a43ba6cdeef33ac
sarabuffalo <sep.14094@yahoo.com>
2018-05-19 16:13:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -26,8 +26,7 @@ function doWhileLoop(array) {
 
 do {
     array = array.slice(1);
-  } while (array.length > 0 || maybeTrue()) 
+  } while (array.length > 0 || maybeTrue())
     return array;
   }
-  
   
